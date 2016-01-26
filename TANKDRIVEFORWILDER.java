3457824e6395ea968aa9efe4df04faf71a18df5d
Steve Cox <stevecox@37093703.school.loyolahs.edu>
2016-01-26 10:39:06 -0800

    update to 4 motor tank drive

--- TANKDRIVEFORWILDER.java
+++ TANKDRIVEFORWILDER.java
@@ -13,18 +13,24 @@ public class TANKDRIVEFORWILDER extends OpMode {
 
     DcMotor drive1;
     DcMotor drive2;
+    DcMotor drive3;
+    DcMotor drive4;
 
     @Override
     public void init(){
         digital = hardwareMap.digitalChannel.get("touch1");
         drive1 = hardwareMap.dcMotor.get("d1");
         drive2 = hardwareMap.dcMotor.get("d2");
+        drive3 = hardwareMap.dcMotor.get("d3");
+        drive4 = hardwareMap.dcMotor.get("d4");
     }
     @Override
     public void loop() {
         boolean digVal = digital.getState();
         telemetry.addData("Digital1", String.format("%1d", (digVal ? 1 : 0)));
-        drive1.setPower(gamepad1.left_stick_y);
-        drive2.setPower(gamepad1.left_stick_y);
+        drive1.setPower(-gamepad1.left_stick_y);
+        drive2.setPower(-gamepad1.left_stick_y);
+        drive3.setPower(gamepad1.right_stick_y);
+        drive4.setPower(gamepad1.right_stick_y);
     }
 }
